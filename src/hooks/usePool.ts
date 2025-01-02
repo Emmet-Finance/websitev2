bef2eaebcb68d7657fe8c86fd71d4c4ea5ec2f2b
Mithras <dimabrook79@gmail.com>
2025-01-02 10:32:24 +0200

    refactor: getData to a separate hook

--- src/hooks/usePool.ts
+++ src/hooks/usePool.ts
@@ -12,22 +12,12 @@ import {
 import { chainFactory } from "../store/chainFactory";
 import { useEthersSigner } from "./useEthersSigner";
 import {
-  setPoolApy,
   setPoolBalance,
-  setPoolDataLoading,
-  setPoolFeeDecimals,
-  setPoolFeeGrowthGlobal,
-  setPoolLiquidityInUSD,
-  setPoolPendingRewards,
-  setPoolProtocolFee,
-  setPoolProtocolFeeAmount,
   setPoolStakedBalance,
-  setPoolTokenFee,
-  setPoolTotalSupply,
 } from "../store/poolSlice";
 import { AddressBookKeys } from "emmet.js";
 import { useTonConnect } from "./useTonConnect";
-import { removeTrailingZeroes, sleep } from "../utils";
+import { sleep } from "../utils";
 
 export default function usePool() {
 
@@ -42,129 +32,9 @@ export default function usePool() {
 
   const pool = useAppSelector((state) => state.pool);
   const bridge = useAppSelector((state) => state.bridge);
-  const [cache, setCache] = useState({});
 
   const [error, setError] = useState("");
-
-  const memoize = (fn: any) => {
-    return async (...args: any) => {
-      let n = args; // just taking one argument here
-      // console.log({ n });
-
-      if (n in cache) {
-        // console.log("Fetching from cache");
-        //@ts-ignore
-        return cache[n];
-      } else {
-        // console.log("Calculating result");
-        let result = await fn(...args);
-        //@ts-ignore
-        cache[n] = result;
-        setCache((prevCache) => {
-          //@ts-ignore
-          prevCache[n] = result;
-          return { ...prevCache };
-        });
-        return result;
-      }
-    };
-  };
-
-  const getData = async (
-    chain = pool.chain,
-    token = pool.token,
-    senderAddress = bridge.senderAddress,
-  ) => {
-    await sleep(10000);
-    try {
-      const handler = await chainFactory.inner(
-        // @ts-ignore
-        ChainToDestinationDomain[ChainNameToTypeChainName[chain]],
-      );
-
-      if ("address" in handler) {
-        const poolAddress = await handler.address(`elp${token}`);
-        // console.log({ poolAddress });
-
-        const decimals = await handler.decimals(poolAddress).catch(() => 1);
-
-        const totalSupply = await handler
-          .getLpTotalSupply(poolAddress)
-          .catch(() => BigInt(0));
-
-        const apy = await handler
-          .getLpCurrentAPY(poolAddress)
-          .catch(() => BigInt(0));
-
-        const protocolFee = await handler
-          .getLpProtocolFee(poolAddress)
-          .catch(() => BigInt(0));
-
-        const protocolFeeAmount = await handler
-          .getLpProtocolFeeAmount(poolAddress)
-          .catch(() => BigInt(0));
-
-        const tokenFee = await handler
-          .getLpTokenFee(poolAddress)
-          .catch(() => BigInt(0));
-
-        const feeGrowthGlobal = await handler
-          .getLpFeeGrowthGlobal(poolAddress)
-          .catch(() => BigInt(0));
-
-        const feeDecimals = await handler
-          .getLpFeeDecimals(poolAddress)
-          .catch(() => BigInt(0));
-
-        const validAddress = await handler.validateAddress(senderAddress);
-
-        const pendingRewards = await handler
-          .getLpProviderRewards(poolAddress, senderAddress)
-          .catch(() => BigInt(0));
-
-        const tokenPrice = await chainFactory.getTokenPrice(token);
-
-        const tokenPriceDecimals =
-          await chainFactory.getPriceDecimals(token);
-
-        const liquidityPoolInUSD =
-          Number(totalSupply * tokenPrice) /
-          10 ** (decimals + Number(tokenPriceDecimals));
-
-        return {
-          decimals,
-          apy: Number(apy) / 100,
-          totalSupply: Number(totalSupply) / 10 ** decimals,
-          protocolFee: Number(protocolFee),
-          protocolFeeAmount: Number(protocolFeeAmount),
-          tokenFee: Number(tokenFee),
-          feeGrowthGlobal: Number(feeGrowthGlobal) / 10 ** decimals,
-          feeDecimals: Number(feeDecimals),
-          pendingRewards: validAddress
-            ? Number(pendingRewards) / 10 ** decimals
-            : 0,
-          liquidityPoolInUSD: Number(liquidityPoolInUSD).toFixed(2),
-        };
-      }
-    } catch (error: { message: string } | any) {
-      setError(error.message);
-      console.error(error);
-      return {
-        decimals: 1,
-        apy: 0,
-        totalSupply: 0,
-        protocolFee: 0,
-        protocolFeeAmount: 0,
-        tokenFee: 0,
-        feeGrowthGlobal: 0,
-        feeDecimals: 0,
-        pendingRewards: 0,
-        liquidityPoolInUSD: 0,
-      };
-    }
-  };
-
-  // const getData = memoize(_getData);
+  const [isLoadingBalance, setIsLoadingBalance] = useState<boolean>(false);
 
   const stake = async () => {
     const handler = await chainFactory.inner(
@@ -195,7 +65,6 @@ export default function usePool() {
       console.error(error);
       setError(error.message);
     }
-    await getData();
   };
 
   const withdraw = async () => {
@@ -227,7 +96,6 @@ export default function usePool() {
       console.error(error);
       setError(error.message);
     }
-    await getData();
   };
 
   const withdrawFees = async () => {
@@ -255,7 +123,6 @@ export default function usePool() {
       console.error(error);
       setError(error.message);
     }
-    await getData();
   };
 
   const getBalance = async (
@@ -281,15 +148,11 @@ export default function usePool() {
         if (type === "Deposit") {
           const tokenAddress = await handler.address(token as AddressBookKeys);
 
-          // console.log({
-          //   balance: Number(await handler.tokenBalance(tokenAddress, address)),
-          // });
-
           return (
             Number(await handler.tokenBalance(tokenAddress, address)) /
             10 ** Number(TOKEN_DECIMALS[token as TTokenName])
           );
-        } else {
+        } else { // Withdraw
           const tokenAddress = await handler.address(
             `elp${token}` as AddressBookKeys,
           );
@@ -310,76 +173,52 @@ export default function usePool() {
   useEffect(() => {
     let interval: NodeJS.Timeout;
 
-    (async () => {
-      dispatch(setPoolBalance(0));
-      dispatch(setPoolStakedBalance(0));
-      dispatch(setPoolApy(0));
-      dispatch(setPoolTotalSupply(0));
-      dispatch(setPoolProtocolFee(0));
-      dispatch(setPoolProtocolFeeAmount(0));
-      dispatch(setPoolTokenFee(0));
-      dispatch(setPoolFeeGrowthGlobal(0));
-      dispatch(setPoolFeeDecimals(0));
-      dispatch(setPoolPendingRewards(0));
-      dispatch(setPoolLiquidityInUSD("0"));
-      if (pool.chain && pool.token && isPoolPath) {
-        if (bridge.senderAddress) {
-          interval = setInterval(async () => {
-            try {
-              const balance = await getBalance(
-                "Deposit",
-                pool.chain,
-                pool.token,
-                bridge.senderAddress,
-              );
-              dispatch(setPoolBalance(balance));
-
-              const stakedBalance = await getBalance(
-                "Withdraw",
-                pool.chain,
-                pool.token,
-                bridge.senderAddress,
-              );
-              dispatch(setPoolStakedBalance(stakedBalance));
-            } catch (error) {
-              clearInterval(interval);
-            }
-          }, 5 * 1000);
-        }
-
-        dispatch(setPoolDataLoading(true));
-        const data = await getData(
+    const fetchData = async () => {
+      try {
+        // Start the spinner
+        setIsLoadingBalance(true);
+  
+        const balance = await getBalance(
+          "Deposit",
           pool.chain,
           pool.token,
           bridge.senderAddress,
         );
+        dispatch(setPoolBalance(balance));
+  
+        const stakedBalance = await getBalance(
+          "Withdraw",
+          pool.chain,
+          pool.token,
+          bridge.senderAddress,
+        );
+        dispatch(setPoolStakedBalance(stakedBalance));
+  
+        // Stop the spinner
+        setIsLoadingBalance(false);
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+        clearInterval(interval);
+      }
+    };
 
-        // console.log({ data, chain: pool.chain, token: pool.token });
-        if (data) {
-          dispatch(setPoolApy(data.apy));
-          dispatch(setPoolTotalSupply(data.totalSupply));
-          dispatch(setPoolProtocolFee(data.protocolFee));
-          dispatch(setPoolProtocolFeeAmount(data.protocolFeeAmount));
-          dispatch(setPoolTokenFee(data.tokenFee));
-          dispatch(setPoolFeeGrowthGlobal(data.feeGrowthGlobal));
-          dispatch(setPoolFeeDecimals(data.feeDecimals));
-          dispatch(setPoolPendingRewards(data.pendingRewards));
-          dispatch(setPoolLiquidityInUSD(data.liquidityPoolInUSD));
+
+
+    (async () => {
+      
+      if (pool.chain && pool.token && isPoolPath) {
+        if (bridge.senderAddress) {
+          interval = setInterval(fetchData, 60_000);
         }
-        dispatch(setPoolDataLoading(false));
       }
     })();
 
     return () => clearInterval(interval);
   }, [pool.chain, pool.token, bridge.senderAddress]);
 
-  useEffect(() => {
-    // console.log({ token: pool.token });
-  }, [pool.token]);
-
   return {
     error,
-    getData,
+    isLoadingBalance,
     stake,
     withdraw,
     getBalance,
