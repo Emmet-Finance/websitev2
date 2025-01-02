710eeccccb7ae5246492960acc5a875666d55f27
Mithras <dimabrook79@gmail.com>
2025-01-02 10:37:13 +0200

    add: spinner awaiting balance

--- src/Pages/PoolComponents/WalletBalance/WalletBalance.jsx
+++ src/Pages/PoolComponents/WalletBalance/WalletBalance.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import Wallet from "../../../assets/img/Wallet.svg";
 import "./WalletBalance.css";
 import { useAppSelector } from "../../../hooks/storage";
+import usePool from "../../../hooks/usePool";
+import ButtonSpinner from "../../CommonComponents/Spinner/ButtonSpinner";
 
 export default function WalletBalance({ name, activeButton }) {
+
   const pool = useAppSelector((state) => state.pool);
+
+  const { isLoadingBalance } = usePool();
+
   return (
     <div className="walletBalance">
       <img src={Wallet} alt="Wallet" />
       <span>
-        {activeButton === "Deposit" ? pool.balance : pool.stakedBalance}
+        {activeButton === "Deposit"
+          ? (isLoadingBalance
+            ? <ButtonSpinner />
+            : pool.balance)
+          : (isLoadingBalance
+            ? <ButtonSpinner />
+            : pool.stakedBalance)
+        }
       </span>
       <span>{name}</span>
     </div>
