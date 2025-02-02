25acf1f42f0bd7e6f91f1a64ad149fefdd7a7ce1
Mithras <dimabrook79@gmail.com>
2025-02-02 12:44:08 +0200

    refactor: LnM supported chains location

--- src/hooks/chains.ts
+++ src/hooks/chains.ts
@@ -1,85 +1,19 @@
-const chains = [
-  {
-    id: 43114,
-    name: "Avalanche",
-    icon: "img/chain/avalanche.svg",
-    domain: 1,
-    requiresApproval: true,
-    nativeCurrency: { name: "AVAX", symbol: "AVAX", decimals: 18 },
-  },
-  {
-    id: 137,
-    name: "Polygon",
-    icon: "img/chain/polygon.svg",
-    domain: 7,
-    requiresApproval: true,
-    nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
-  },
-  {
-    id: 80002,
-    name: "Amoy",
-    icon: "img/chain/polygon.svg",
-    domain: 7,
-    requiresApproval: true,
-    nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
-  },
-  {
-    id: 65535,
-    name: "TONTestnet",
-    icon: "img/chain/ton.svg",
-    domain: 65535,
-    requiresApproval: false,
-    nativeCurrency: { name: "TON", symbol: "TON", decimals: 9 },
-  },
-  {
-    id: 11155111,
-    name: "Sepolia",
-    icon: "img/chain/ethereum.svg",
-    domain: 0,
-    requiresApproval: true,
-    nativeCurrency: { name: "ETH", symbol: "ETH", decimals: 18 },
-  },
-  {
-    id: 97,
-    name: "BSCTestnet",
-    icon: "img/chain/bsc.svg",
-    domain: 4,
-    requiresApproval: true,
-    nativeCurrency: { name: "TBNB", symbol: "TBNB", decimals: 18 },
-  },
-  {
-    id: 56,
-    name: "BSC",
-    icon: "img/chain/bsc.svg",
-    domain: 56,
-    requiresApproval: true,
-    nativeCurrency: {name: "BNB", symbol: "BNB", decimals: 18 }
-  },
-  {
-    id: 80084,
-    name: "Bartio",
-    icon: "img/chain/berachain.png",
-    domain: 80084,
-    requiresApproval: true,
-    nativeCurrency: { name: "BERA", symbol: "BERA", decimals: 18 },
-  },
-  {
-    id: 728696,
-    name: "OnlyTestnet",
-    icon: "img/chain/berachain.png",
-    domain: 728696,
-    requiresApproval: true,
-    nativeCurrency: { name: "ONLY", symbol: "ONLY", decimals: 18 },
-  },
-  {
-    id: 65534,
-    name: "TON",
-    icon: "img/chain/ton.png",
-    domain: 65534,
-    requiresApproval: false,
-    nativeCurrency: { name: "TON", symbol: "TON", decimals: 9 },
-  },
-] as const;
+import chainData from "../data/lockAndMintChain.json";
+
+export type TLnMChain = {
+  id: number,
+  name: string,
+  icon: string,
+  domain: number,
+  requiresApproval: boolean,
+  nativeCurrency: {
+    name: string,
+    symbol: string,
+    decimals: number
+  }
+}
+
+const chains: TLnMChain[] = chainData;
 
 export type LockAndMintSupportedChainIDs = (typeof chains)[number]["domain"];
 
