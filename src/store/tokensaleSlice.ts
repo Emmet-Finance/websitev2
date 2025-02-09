50896345bff7a367d6e52fcef2527ff156e13c55
Mithras <dimabrook79@gmail.com>
2025-02-09 18:58:31 +0200

    add: setPay logic & setAllowance

--- src/store/tokensaleSlice.ts
+++ src/store/tokensaleSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ITokenSale {
+    allowance: number;
     buyer: string;
     balance: number;
     pay: number;
@@ -8,6 +9,7 @@ interface ITokenSale {
 }
 
 const initialState = {
+    allowance: 0,
     buyer: "",
     balance: 0,
     pay: 0,
@@ -18,22 +20,41 @@ export const tokensaleSlice = createSlice({
     name: "tokensale",
     initialState,
     reducers: {
-        setBuyer(state: ITokenSale, action: PayloadAction<string>){
+        setAllowance(state: ITokenSale, action: PayloadAction<number>) {
+            state.allowance = action.payload;
+        },
+        setBuyer(state: ITokenSale, action: PayloadAction<string>) {
             state.buyer = action.payload;
         },
-        setBalance(state: ITokenSale, action: PayloadAction<number>){
+        setBalance(state: ITokenSale, action: PayloadAction<number>) {
             state.balance = action.payload;
         },
-        setPay(state: ITokenSale, action: PayloadAction<number>){
+        setPay(state: ITokenSale, action: PayloadAction<number>) {
             state.pay = action.payload;
+            if(Number(state.pay) < 20) {
+                state.receive = 0;
+            } else if (Number(state.pay) < 1_000) {
+                state.receive = Number(state.pay) * 100;
+            } else if (Number(state.pay) >= 1_000 && Number(state.pay) < 5_000) {
+                state.receive = Number(state.pay) * 102;
+            } else if (Number(state.pay) >= 5_000 && Number(state.pay) < 20_000) {
+                state.receive = Number(state.pay) * 103;
+            } else if (Number(state.pay) >= 20_000 && Number(state.pay) < 50_000) {
+                state.receive = Number(state.pay) * 105;
+            } else if (Number(state.pay) >= 50_000 && Number(state.pay) < 100_000) {
+                state.receive = Number(state.pay) * 108;
+            } else if (Number(state.pay) >= 100_000) {
+                state.receive = Number(state.pay) * 110;
+            }
         },
-        setReceive(state: ITokenSale, action: PayloadAction<number>){
+        setReceive(state: ITokenSale, action: PayloadAction<number>) {
             state.receive = action.payload;
         }
     }
 });
 
 export const {
+    setAllowance,
     setBuyer,
     setBalance,
     setPay,
