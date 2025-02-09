4b275b89f10c936dcf34ec0b8e834c53af597450
Mithras <dimabrook79@gmail.com>
2025-02-09 19:13:23 +0200

    update: tokenomics box

--- src/Pages/WebPages/TokenomicsComponents/TokenomicsBox.jsx
+++ src/Pages/WebPages/TokenomicsComponents/TokenomicsBox.jsx
@@ -9,8 +9,16 @@ import Farming from "../../../assets/img/web/Tokenomics/unity/5.svg";
 import Borrow from "../../../assets/img/web/Tokenomics/unity/6.svg";
 import Voting from "../../../assets/img/web/Tokenomics/unity/7.svg";
 
-import Etheriam from "../../../assets/img/web/Tokenomics/etheriam.svg";
+import BSC from "../../../assets/img/bsc.svg";
 import CopyAddress from "../../../assets/img/web/Tokenomics/Copy.svg";
+
+const emmetAddress = "0x6b30f76CecE9F92D27f0e9Ad78312E77709E74A5";
+
+const handleCopyClick = () => {
+  (async () => {
+    await navigator.clipboard.writeText(emmetAddress);
+  })();
+}
 
 function TokenomicsBox() {
   return (
@@ -65,11 +73,16 @@ function TokenomicsBox() {
                     <div className="metricsLeft">Token Address</div>
                     <div className="metricsRight">
                       <div className="metricAddress">
-                        <img src={Etheriam} alt="Etheriam" />
-                        <p className="linkAddress">0x96...0dD1</p>
-                        <span className="copyAddressLink">
-                          <img src={CopyAddress} alt="Copy" />
-                        </span>
+                        <img src={BSC} alt="BSC" style={{ "width": "30px" }} />
+                        <a href="https://bscscan.com/address/0x6b30f76CecE9F92D27f0e9Ad78312E77709E74A5" target="_blank" rel="noopener noreferrer">
+                          <p className="linkAddress">{`${emmetAddress.slice(0, 4)}...${emmetAddress.slice(38,)}`}</p>
+                        </a>
+                        <button className="copyLink" onClick={() => handleCopyClick()}>
+                          <span className="copyAddressLink">
+                            <img src={CopyAddress} alt="Copy" />
+                          </span>
+                        </button>
+
                       </div>
                     </div>
                   </li>
@@ -85,24 +98,24 @@ function TokenomicsBox() {
                   </li>
                   <li>
                     <div className="metricsLeft">Allocated Supply</div>
-                    <div className="metricsRight">2% (20,000,000 $EMMET)</div>
+                    <div className="metricsRight">8% (80,000,000 $EMMET)</div>
                   </li>
                   <li>
                     <div className="metricsLeft">Hard Cap</div>
-                    <div className="metricsRight">$200,000</div>
+                    <div className="metricsRight">$800,000</div>
                   </li>
                   <li>
                     <div className="metricsLeft">Fully Diluted Value</div>
                     <div className="metricsRight">$10,000,000</div>
                   </li>
                   <li>
-                    <div className="metricsLeft">Purchase Limit</div>
-                    <div className="metricsRight">$5,000$ - $100,000</div>
+                    <div className="metricsLeft">Purchase Minimum Limit</div>
+                    <div className="metricsRight">$20$</div>
                   </li>
                   <li>
                     <div className="metricsLeft">Lockup Terms</div>
                     <div className="metricsRight">
-                      TGE (25%) / Vesting (75%) - 36 mon
+                      Imediately (40%) / Vesting (60%) - 6 mon
                     </div>
                   </li>
                 </ul>
