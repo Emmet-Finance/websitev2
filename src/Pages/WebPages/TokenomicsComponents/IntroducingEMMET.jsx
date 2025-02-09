b151ed99f6f47f42a29675ff1d92491436f90ba5
Mithras <dimabrook79@gmail.com>
2025-02-09 19:07:24 +0200

    split: mobile & desktop views

--- src/Pages/WebPages/TokenomicsComponents/IntroducingEMMET.jsx
+++ src/Pages/WebPages/TokenomicsComponents/IntroducingEMMET.jsx
@@ -1,42 +1,71 @@
 import React from "react";
-
+import { isMobile } from "react-device-detect";
 import "./IntroducingEMMET.css";
-import IntroArt from "../../../assets/img/web/Tokenomics/IntroArt.png";
 import BuyEmmet from "./BuyEmmet";
+import PricesTable from "./PricesTable";
+import RefComponent from "./RefComponent";
 
 function IntroducingEMMET() {
+
   return (
     <div className="introEmmetContainer">
       <div className="container introEmmetContent">
-        <img src={IntroArt} alt="IntroArt" className="introArt" />
-        <div className="row">
-          <div className="col-lg  -6">
-            <div className="introEmmetLeft">
-              <h1>
-                Introducing <br />
-                <span>$EMMET</span>
-              </h1>
-              <p>
-                EMMET native token unlocks the power of decentralized finance,
-                giving you access to a growing ecosystem of cutting-edge DeFi
-                tools and innovative blockchain applicatios.
-              </p>
-              <a
-                href="https://doc.emmet.finance/docs/Tokenomics/"
-                className="whiteBtn"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Explore $EMMET Tokenomics
-              </a>
-            </div>
-          </div>
-          <div className="col-lg  -6">
-            <div className="introEmmetRight">
-              <BuyEmmet />
-            </div>
-          </div>
-        </div>
+        {
+          isMobile
+            ? (<div className="row">
+              <div className="col-lg  -6">
+                <div className="introEmmetRight">
+                  <BuyEmmet />
+                </div>
+              </div>
+              <div className="col-lg  -6">
+                <div className="introEmmetLeft">
+                <p></p>
+                <h2>Refer a friend</h2>
+                <p></p>
+                  <RefComponent />
+                  <p></p>
+                  <h1 className="align-center">
+                    Introducing <br />
+                    <span>$EMMET</span>
+                  </h1>
+                  <p>
+                    EMMET native token unlocks the power of decentralized finance,
+                    giving you access to a growing ecosystem of cutting-edge DeFi
+                    tools and innovative blockchain applicatios.
+                  </p>
+                  <PricesTable />
+
+                </div>
+              </div>
+            </div>)
+            : (<div className="row">
+              <div className="col-lg  -6">
+                <div className="introEmmetLeft">
+                  <h1 className="align-center">
+                    Introducing <br />
+                    <span>$EMMET</span>
+                  </h1>
+                  <p>
+                    EMMET native token unlocks the power of decentralized finance,
+                    giving you access to a growing ecosystem of cutting-edge DeFi
+                    tools and innovative blockchain applicatios.
+                  </p>
+                  <PricesTable />
+                  <p></p>
+                  <RefComponent />
+                </div>
+              </div>
+
+              <div className="col-lg  -6">
+                <div className="introEmmetRight">
+                  <BuyEmmet />
+                </div>
+              </div>
+            </div>)
+        }
+
+
       </div>
     </div>
   );
