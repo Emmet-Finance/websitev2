be9f0202185a7a594ba6930cc986cec47b93a46a
Mithras <dimabrook79@gmail.com>
2025-02-09 19:15:45 +0200

    update distribution

--- src/Pages/WebPages/TokenomicsComponents/Distribution.jsx
+++ src/Pages/WebPages/TokenomicsComponents/Distribution.jsx
@@ -20,52 +20,64 @@ function Distribution() {
                 <h4>Investors</h4>
                 <ul>
                   <li>
-                    <span className="colorBox pseed"></span> <span>2%</span>{" "}
-                    Pre-Seed
+                    <span className="colorBox pseed"></span> <span>8%</span>{" "}
+                    Pre-Seed (open)
                   </li>
                   <li>
-                    <span className="colorBox prse"></span> <span>2%</span>{" "}
+                    <span className="colorBox prse"></span> <span>5%</span>{" "}
                     Private Seed
-                  </li>
-                  <li>
-                    <span className="colorBox comsal"></span> <span>2%</span>{" "}
-                    Community Sale
                   </li>
                   <li>
                     <span className="colorBox stratInv"></span> <span>5%</span>{" "}
                     Strategic Investors
                   </li>
                   <li>
+                    <span className="colorBox comsal"></span> <span>1%</span>{" "}
+                    KOLs (influencers)
+                  </li>
+                  <li>
                     <span className="colorBox publicSale"></span>{" "}
-                    <span>1%</span> Public Sale: IDO (TGE)
+                    <span>1.5%</span> Launchpad Sale
+                  </li>
+                  <li>
+                    <span className="colorBox publicSale"></span>{" "}
+                    <span>7%</span> CEX/DEX Listing (IDO)
                   </li>
                 </ul>
               </div>
               <div className="distributionBox">
                 <h4>Ecosystem</h4>
                 <ul>
-                  <li>
-                    <span className="colorBox teamCore"></span> <span>14%</span>{" "}
-                    Team Core
+                <li>
+                    <span className="colorBox publicSale"></span> <span>2%</span>{" "}
+                    Advisors
                   </li>
                   <li>
-                    <span className="colorBox marketingPar"></span>{" "}
-                    <span>14%</span> Marketing & Partnerships
+                    <span className="colorBox teamCore"></span> <span>12%</span>{" "}
+                    Team
                   </li>
                   <li>
-                    <span className="colorBox liquiRew"></span> <span>20%</span>{" "}
-                    Liquidity Rewards
+                    <span className="colorBox marketingPar "></span> <span>7%</span>{" "}
+                    Cross-chain growth
+                  </li>
+                  <li>
+                    <span className="colorBox liquiRew"></span>{" "}
+                    <span>10%</span> Marketing
                   </li>
                   <li>
                     <span className="colorBox stakingRew"></span>{" "}
-                    <span>20%</span> Staking Rewards
+                    <span>7.5%</span> Treasury
                   </li>
                   <li>
-                    <span className="colorBox echoSys"></span> <span>20%</span>{" "}
-                    Ecosystem Fund
+                    <span className="colorBox echoSys"></span> <span>7%</span>{" "}
+                    Staking rewards
                   </li>
                   <li>
-                    <span className="colorBox airdrop"></span> <span>1%</span>{" "}
+                    <span className="colorBox echoSys"></span> <span>24%</span>{" "}
+                    Strategic reserve
+                  </li>
+                  <li>
+                    <span className="colorBox airdrop"></span> <span>3%</span>{" "}
                     Airdrop
                   </li>
                 </ul>
