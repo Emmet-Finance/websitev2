3746477295d9f2491bb6c1ee5c58a593d45b7d94
Mithras <dimabrook79@gmail.com>
2025-02-18 07:06:01 +0200

    start implementing staking

--- src/Pages/Staking/Stake.jsx
+++ src/Pages/Staking/Stake.jsx
@@ -1,13 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAccount } from "wagmi";
+import { useAppKit } from '@reown/appkit/react';
+import { useAppSelector, useAppDispatch } from '../../hooks/storage';
+import ButtonSpinner from "../CommonComponents/Spinner/ButtonSpinner";
+import { modal } from "../../App";
+import { findChainfromName } from "../../utils";
+import useTabVisibility from "../../hooks/useTabVisibility";
+import useStaking from "../../hooks/useStaking";
+import { setAmount, setPeriod } from "../../store/stakingSlice";
+
+const insufficientBalance = "Insufficient balance"
+const captionApprove = "Approve";
+const captionBuy = "Buy Tokens";
+const connectWallet = "Connect your BSC wallet";
 
 function Stake() {
+  const staking = useAppSelector(state => state.staking);
+  const { open } = useAppKit();
+  const dispatch = useAppDispatch();
+  const { isConnected, account } = useAccount();
+  const { approve, isAwaiting, txHash, stake, unstake, withdrawRewards } = useStaking();
+
   // State to track the active staking item
-  const [activeItem, setActiveItem] = useState(null);
+  const [activeItem, setActiveItem] = useState(3);
+  const [caption, setCaption] = useState("Enter Amount");
+  const [showSpinner, setShowSpiner] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [amount, setStakeAmount] = useState("");
+  const [oldAmount, setOldAmount] = useState("");
 
   // Function to handle item click
   const handleItemClick = (index) => {
     setActiveItem(index);
+    dispatch(setPeriod(index));
   };
+
+  const inputStakeChange = (e) => {
+    e.preventDefault();
+    let inputValue = e.target.value.replace(/\D/g, "");
+    if (inputValue) {
+      setStakeAmount(Number(inputValue).toFixed(0));
+    } else {
+      setStakeAmount("");
+      setOldAmount("");
+    }
+  }
+
+  const maxClickHandle = () => {
+    dispatch(setAmount(staking.balance));
+  }
+
+  useEffect(() => {
+    if (amount) {
+      setOldAmount(amount);
+      dispatch(setAmount(amount))
+    } else {
+      dispatch(setAmount(oldAmount))
+    }
+  }, [amount])
 
   return (
     <div className="stake stakeBox">
@@ -16,11 +66,19 @@ function Stake() {
         <div className="emmentAmount">
           <div className="emmentAmountTitle">
             <p className="grayText">Enter $EMMET amount</p>
-            <h4>892.06 EMMET Available</h4>
+            <h4>{staking.balance ? staking.balance.toLocaleString() : 0} EMMET Available</h4>
           </div>
           <div className="enter_amount">
-            <input type="number" value="50" placeholder="" />
-            <span className="max_amount">MAX</span>
+            <input
+              type="number"
+              value={staking.amount ? staking.amount : ""}
+              placeholder=""
+              onChange={e => inputStakeChange(e)}
+            />
+            <span
+              className="max_amount"
+              onClick={maxClickHandle}
+            >MAX</span>
           </div>
         </div>
         <div className="selectStaking">
@@ -47,12 +105,18 @@ function Stake() {
           </ul>
           <ul className="stakTotal stakList">
             <li>
-              <p className="grayText">End Date</p>
-              <div>Estimated Rewards</div>
+              <p className="grayText">Maturity Date</p>
+              <div>{`${new Date(Date.now() + (activeItem+1) * 3 * 30.5 * 24 * 60 * 60 * 1000).toDateString()}`}</div>
             </li>
             <li>
-              <p className="grayText">End Date</p>
-              <div>358 EMMET <span>$ 0.070</span></div>
+              <p className="grayText">Estimated Rewards</p>
+              <div>{staking.estimatedReward
+                ? staking.estimatedReward.toLocaleString() + ` EMMET`
+                : 0 + ` EMMET`}
+                <span> {staking.estimatedReward
+                  ? `$${Number(staking.estimatedReward) * 0.01}`
+                  : <>$ 0.00</>
+                  } </span></div>
             </li>
           </ul>
         </div>
