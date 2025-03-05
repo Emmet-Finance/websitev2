eb34e8f2f54aead4444c64044d8430acbacaabc5
Mithras <dimabrook79@gmail.com>
2025-03-05 10:22:20 +0200

    add: Zerro Fees Banner

---
+++ src/Pages/Staking/ZeroFeesBanner.jsx
@@ -0,0 +1,19 @@
+import "./ZeroFeesBanner.css";
+
+
+export default function ZeroFeeBanner() {
+
+    return (
+        <div className="card">
+            <span>
+                Zero Staking Fees for $EMMET Holders!
+            </span>
+            <span>
+                <a href="/tokensale">
+                    Buy $EMMET
+                </a>
+            </span>
+
+        </div>
+    )
+}
