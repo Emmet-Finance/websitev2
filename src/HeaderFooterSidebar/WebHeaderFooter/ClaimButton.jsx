ef1215fe7d074ba1e98e8811fa2e860d47fcf8d2
Mithras <dimabrook79@gmail.com>
2025-03-22 12:42:43 +0200

    add: Claim button

---
+++ src/HeaderFooterSidebar/WebHeaderFooter/ClaimButton.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import useTokenSale from "../../hooks/useTokenSale";
+import AlertModal from "./AlertModal";
+
+export default function ClaimButton() {
+
+    const [msg, setMsg] = useState("");
+    const [alertIsOpen, setAlertIsOpen] = useState(false);
+
+    const { error, setError, claimEmmet } = useTokenSale();
+
+    const handleClaimClick = () => {
+        claimEmmet()
+    }
+
+    useEffect(() => {
+
+        if (error) {
+            setMsg(error);
+            setAlertIsOpen(true);
+        } else {
+            setMsg("");
+            setAlertIsOpen(false);
+        }
+
+    }, [error]);
+
+    function closedAlert() {
+        setMsg("");
+        setAlertIsOpen(false);
+        setError("")
+    };
+
+    return (<>
+        <AlertModal
+            msg={msg}
+            alertIsOpen={alertIsOpen}
+            closedAlert={() => closedAlert()}
+        />
+        <div
+            className="launchApp pointer"
+            onClick={handleClaimClick}
+        >
+            Claiming
+        </div>
+    </>)
+
+}
